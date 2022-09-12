8e247abc3bdd8b34d0f752879d181bd2558e1382
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-09-12 01:48:44 +0100

    pushing published

---
+++ out/_next/static/xOpplQQ132zMXNKycCtZh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/428-111447d1af071855.js","static/css/cc11e808671a5477.css","static/chunks/pages/index-1a44dfa0bbd4bd0f.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
